78d37f4344f6e89904f703f16942707998293ae6
Josh Hamit <josh.hamit@gmail.com>
2012-10-03 07:07:06 -0500

    first commit - including interactive example

---
+++ interactive.groovy
@@ -0,0 +1,12 @@
+import groovy.grape.Grape
+Grape.grab([group:'org.codehaus.geb', module:'geb-core', version:'0.7.2'])
+Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-firefox-driver', version:'2.25.0'])
+Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-support',version:'2.25.0'])
+import geb.*
+import java.util.logging.*
+
+new File("interactive.properties").withInputStream {
+    LogManager.logManager.readConfiguration it }
+
+b = new Browser(baseUrl: "https://twitter.com/dfw2gug")
+b.go()
