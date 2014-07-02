c23b0f1200f0347901054f1a0a195a9239f219d1
Josh Hamit <josh.hamit@gmail.com>
2014-07-02 13:15:13 -0500

    updated some version numbers

--- interactive.groovy
+++ interactive.groovy
@@ -1,7 +1,7 @@
 import groovy.grape.Grape
 Grape.grab([group:'org.codehaus.geb', module:'geb-core', version:'0.7.2'])
-Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-firefox-driver', version:'2.25.0'])
-Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-support',version:'2.25.0'])
+Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-firefox-driver', version:'2.39.0'])
+Grape.grab([group:'org.seleniumhq.selenium', module:'selenium-support',version:'2.39.0'])
 import geb.*
 import java.util.logging.*
 
